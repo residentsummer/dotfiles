70d6b31403c7e3aa8f9fe7c9f4c9de65ec301223
Anton S <residentsummer@gmail.com>
2015-10-21 11:11:26 +0300

    Zsh: used plugin for ctrl-z resume

--- .config/zsh/custom.zsh
+++ .config/zsh/custom.zsh
@@ -1,18 +1,5 @@
 # Some personal shell customizations
 
-# Ctrl-Z in shell resumes last suspended job
-fancy-ctrl-z () {
-  if [[ $#BUFFER -eq 0 ]]; then
-    BUFFER="fg"
-    zle accept-line
-  else
-    zle push-input
-    zle clear-screen
-  fi
-}
-zle -N fancy-ctrl-z
-bindkey '^Z' fancy-ctrl-z
-
 mkcd () {
     mkdir -p $1
     cd $1
