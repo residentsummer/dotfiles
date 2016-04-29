7011ff6d2d329085b4841947436640d4d89b065c
Anton S <residentsummer@gmail.com>
2016-04-29 22:53:14 +0800

    Zsh: no need for DOCKER_HOST with dlite

--- .config/zsh/env.sh
+++ .config/zsh/env.sh
@@ -4,7 +4,8 @@ export EDITOR=vim
 export PYTHONSTARTUP="$HOME/.pyrc"
 export PYTHONIOENCODING="UTF-8"
 
-export DOCKER_HOST=tcp://localhost:2375
+# dlite is great!
+# export DOCKER_HOST=tcp://localhost:2375
 
 if [ -f $ZSH_CUSTOM/local-env.sh ]; then
     source $ZSH_CUSTOM/local-env.sh
