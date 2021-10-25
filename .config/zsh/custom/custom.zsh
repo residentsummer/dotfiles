a6bad04eb4adeef9b16d5a8c9b5731dec19a883f
Anton S <residentsummer@gmail.com>
2021-10-25 11:39:00 +0300

    aliases: pie - search+replace with perl

--- .config/zsh/custom/custom.zsh
+++ .config/zsh/custom/custom.zsh
@@ -24,3 +24,24 @@ xml_pp () {
     xmllint --noent --encode UTF-8 --format -
 }
 
+pie () {
+    if [[ -z "$1" || -z "$3" ]]; then
+        echo "usage: replace before after file [... file]"
+        echo
+        echo "example:"
+        echo "    replace foo \"\" **/*.py"
+        return 1
+    fi
+
+    local before=$1
+    local after=$2
+    shift 2
+
+    for f in $@; do
+        if [[ -f "$f" ]]; then
+            # perl -pi -e "$M=$_ s/$before/$after/; END{exit 1 unless $M>0}" "$f"
+            perl -pi -e "s/$before/$after/" "$f"
+        fi
+    done
+}
+
