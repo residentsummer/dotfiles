1688e77c583fde1266b3d1971c005c397496f9c0
Anton S <residentsummer@gmail.com>
2022-06-08 13:06:56 +0800

    zsh: better path management

--- .config/zsh/custom/env.sh
+++ .config/zsh/custom/env.sh
@@ -1,4 +1,3 @@
-export PATH=$HOME/.pyenv/shims:/usr/local/bin:$PATH
 export EDITOR=vim
 
 export PYTHONSTARTUP="$HOME/.pyrc"
@@ -8,3 +7,20 @@ if [ -f $ZSH_CUSTOM/local-env.sh ]; then
     source $ZSH_CUSTOM/local-env.sh
 fi
 
+# 3rd-party binaries
+if [[ -n $GRAALVM_HOME ]]; then
+    # NOTE Shadow normal java installation
+    export PATH=$GRAALVM_HOME/bin:$PATH
+fi
+if [[ -d $HOME/.pyenv/shims ]]; then
+    # NOTE Shadow system/brew python(s)
+    export PATH=$HOME/.pyenv/shims:$PATH
+fi
+if [[ -d $HOME/.krew/bin ]]; then
+    export PATH=$PATH:$HOME/.krew/bin
+fi
+
+# Local scripts
+if [[ -d $HOME/Library/bin ]]; then
+    export PATH=$HOME/Library/bin:$PATH
+fi
